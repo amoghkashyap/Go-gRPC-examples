56c7a1821b07a5255f3715cebfd73e909c30b946
akashyap <amogh.kashyap@nokia.com>
2018-01-12 12:21:42 +0530

    DB manager initial commit

---
+++ CassandraDB+Migrations/cassandra/db_manager.go
@@ -0,0 +1,99 @@
+package cassandra
+
+import (
+	"github.com/gocql/gocql"
+	"github.com/mattes/migrate"
+	"github.com/mattes/migrate/database/cassandra"
+	_ "github.com/mattes/migrate/source/file"
+	"sync"
+	"log"
+	"strconv"
+)
+
+type DbManager struct{
+	Session *gocql.Session
+}
+
+const (
+	keyspaceName = "biodata"
+	createkeyspace = "CREATE KEYSPACE IF NOT EXISTS biodata WITH replication = {'calss':SimpleStrategy','replication-factor':1}"
+	port = 9042
+	//migrationFiles = "C:/Go/bin/src/Go-gRPC-examples/CassandraDB+Migrations/migration/cql"
+	migrationFiles = "github.com/amoghkashyap/Go-gRPC-examples/CassandraDB+Migrations/migration/cql"
+	)
+
+var (
+	instance *DbManager
+	once sync.Once
+)
+
+func createInstance() *DbManager{
+	instance = new(DbManager)
+	host := "localhost"
+	instance.handleMigrations(host)
+
+	//create cassandra session
+	cluster := gocql.NewCluster(host)
+	cluster.Port = port
+	cluster.Keyspace = keyspaceName
+	session,err := cluster.CreateSession()
+	if err != nil{
+		log.Fatalf("Unable to connect to cassandra session: %v", err)
+	}
+	instance.Session = session
+	log.Println("Cassandra session Initialized")
+
+	return instance
+
+}
+
+func (db *DbManager) handleMigrations(host string){
+	//create cassandra keyspace
+	createCassandraKeyspace(host)
+
+	cassandra := cassandra.Cassandra{}
+
+	url := "cassandra://"+host+":"+strconv.Itoa(port)+"/"+keyspaceName
+	driver, err := cassandra.Open(url)
+	if err != nil {
+		log.Fatalf("Unable to connect to cassandra:%v", err)
+	}
+	mig, err := migrate.NewWithDatabaseInstance(migrationFiles,"cassandra",driver)
+	if err != nil {
+		log.Fatalf("Failed to validate migration:%v", err)
+	}
+	if err := mig.Up(); err != nil && err !=migrate.ErrNoChange {
+		log.Fatalf("Failed to apply migration %v", err)
+	}
+	//Close the connection and handle error
+	if srcErr, dbErr := mig.Close(); srcErr != nil || dbErr != nil {
+		log.Fatalf("Error while executing the migrations %v %v", srcErr, dbErr)
+	}
+}
+
+func createCassandraKeyspace(host string){
+	//Create cassandra session
+	cluster := gocql.NewCluster(host)
+	cluster.Port = port
+	dbSession, err := cluster.CreateSession()
+	if err != nil{
+		log.Fatalf("Not able to connect to cassandra %v", err)
+	}
+	defer dbSession.Close()
+	//Create keyspace if not exists
+	if err := dbSession.Query(createkeyspace).Exec(); err !=nil{
+		//Close the cassandra session
+		dbSession.Close()
+		log.Fatalf("Failed to create keyspace %v", err)
+	}
+}
+
+func (db *DbManager) close(){
+	db.Session.Close()
+	log.Println("Closing the cassandra session")
+}
+
+
+
+
+
